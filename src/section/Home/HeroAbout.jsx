41a404fdc3d97e2d257c52b6a83ab4311ee47a0a
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-01-19 10:20:44 +0530

    New commit

--- src/section/Home/HeroAbout.jsx
+++ src/section/Home/HeroAbout.jsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HeroSlider from "./HeroSlider";
 
-export default function Hero() {
+export default function HeroAbout() {
   const [backgroundImage, setBackgroundImage] = useState(
     "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   );
+  const [place, setPlace] = useState("Ladakh");
 
-  const handleSliderClick = (imageUrl) => {
+  const handleSliderClick = (imageUrl, place) => {
     setBackgroundImage(imageUrl);
-    // Do something with the selected image URL, such as changing the background
-    console.log("Selected Image:", imageUrl);
+    setPlace(place);
   };
 
   return (
@@ -29,12 +29,16 @@ export default function Hero() {
               </div>
             </div>
           </div>
+          <div className="absolute bottom-0 left-1/2 transform  pb-10 -translate-x-1/2 transition-all ease-in-out duration-500 ">
+            <h1 className="font-CooperHevitt text-4xl drop-shadow-2xl  p-2">
+              {place}
+            </h1>
+          </div>
           <div className="absolute right-0 bottom-0">
             <HeroSlider onSliderClick={handleSliderClick} />
           </div>
         </div>
       </div>
-      <div></div>
     </>
   );
 }
--- src/section/Home/HeroSlider.jsx
+++ src/section/Home/HeroSlider.jsx
@@ -5,22 +5,27 @@ const stories = [
   {
     imageUrl:
       "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    place: "Ladakh",
   },
   {
     imageUrl:
       "https://images.pexels.com/photos/7919/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1000",
+    place: "Himalayas",
   },
   {
     imageUrl:
       "https://images.pexels.com/photos/774861/pexels-photo-774861.jpeg?auto=compress&cs=tinysrgb&w=1000",
+    place: "Rocky Mountains",
   },
   {
     imageUrl:
       "https://images.pexels.com/photos/708921/pexels-photo-708921.jpeg?auto=compress&cs=tinysrgb&w=1000",
+    place: "Andes",
   },
   {
     imageUrl:
       "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
+    place: "Alps",
   },
 ];
 
@@ -69,76 +74,13 @@ export default function HeroSlider({ onSliderClick }) {
     }
   };
 
-  const handleSliderClick = (imageUrl) => {
+  const handleSliderClick = (imageUrl, place) => {
     setSelectedImage(imageUrl);
     if (onSliderClick) {
-      onSliderClick(imageUrl);
+      onSliderClick(imageUrl, place);
     }
   };
-  const moveWrapRef = useRef(null);
-  const project1Ref = useRef(null);
 
-  useEffect(() => {
-    const moveWrap = moveWrapRef.current;
-    const project1 = project1Ref.current;
-
-    if (!moveWrap || !project1) {
-      return;
-    }
-
-    let mouseX = 0,
-      mouseY = 0;
-
-    const handleMouseMove = (e) => {
-      const offset = moveWrap.getBoundingClientRect();
-      mouseX = Math.min(
-        e.pageX - offset.left,
-        offset.width - project1.offsetWidth
-      );
-      mouseY = Math.min(
-        e.pageY - offset.top,
-        offset.height - project1.offsetHeight
-      );
-
-      if (mouseX < 0) mouseX = 0;
-      if (mouseY < 0) mouseY = 0;
-    };
-
-    const handleMouseEnter = () => {
-      project1.style.display = "block";
-    };
-
-    const handleMouseLeave = () => {
-      project1.style.display = "none";
-    };
-
-    document.addEventListener("mousemove", handleMouseMove);
-    moveWrap.addEventListener("mouseenter", handleMouseEnter);
-    moveWrap.addEventListener("mouseleave", handleMouseLeave);
-
-    let xp = 0,
-      yp = 0;
-
-    const loop = () => {
-      xp += (mouseX - xp) / 10;
-      yp += (mouseY - yp) / 10;
-      project1.style.left = xp - 30 + "px";
-      project1.style.top = yp - 30 + "px";
-      requestAnimationFrame(loop);
-    };
-
-    loop();
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      moveWrap.removeEventListener("mouseenter", handleMouseEnter);
-      moveWrap.removeEventListener("mouseleave", handleMouseLeave);
-    };
-  }, []);
-
-  const openModal = () => {
-    console.log("hello");
-  };
   return (
     <>
       <div
@@ -155,28 +97,18 @@ export default function HeroSlider({ onSliderClick }) {
           </button>
 
           <div
-            className="flex items-end justify-start flex-shrink-0 gap-2 w-[400px] text-left bg-center bg-cover h-[150px] overflow-x-auto scroll-smooth overflow-y-hidden transition-all ease-in-out duration-300 "
+            className="flex items-end justify-start flex-shrink-0 gap-2 w-[400px] text-left bg-center bg-cover h-[150px] overflow-x-auto scroll-smooth overflow-y-hidden transition-all ease-in-out duration-300"
             ref={scrollContainerRef}
           >
             {stories.map((story, index) => (
               <div
                 key={index}
-                className="relative flex min-w-[250px] items-end justify-start w-full text-left bg-center bg-cover h-[150px] bg-gray-500 rounded-md group"
+                className="relative flex min-w-[250px] items-end justify-start w-full text-left bg-center bg-cover h-[150px] bg-gray-500 rounded-md cursor-pointer"
                 style={{
                   backgroundImage: `url("${story.imageUrl}")`,
                 }}
-                ref={moveWrapRef}
-                onClick={() => handleSliderClick(story.imageUrl)}
+                onClick={() => handleSliderClick(story.imageUrl, story.place)}
               >
-                <span
-                  id="project1"
-                  ref={project1Ref}
-                  className="absolute transition-all z-[999999] duration-0 text-white opacity-80  backdrop-blur-sm  bg-black p-5 rounded-full  cursor-pointer"
-                  style={{ display: "none" }}
-                  onClick={openModal}
-                >
-                  <span className="backdrop-blur-sm text-xs">View</span>
-                </span>
                 <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b via-transparent from-[#00000090] to-[#00000057] rounded-md"></div>
               </div>
             ))}
