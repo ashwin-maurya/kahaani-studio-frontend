2531a2fcec64fe465450a3bb2c1b97fa15c2fa59
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-01-20 11:14:58 +0530

    New commit

--- src/section/Story/StorySlider.jsx
+++ src/section/Story/StorySlider.jsx
@@ -1,95 +1,96 @@
-import React, { useState, useEffect } from "react";
-import Stories from "react-insta-stories";
-import SeeMore from "./SeeMore";
+import React from "react";
+import ReactInstaStories from "react-insta-stories";
 
-export default function StorySlider() {
-  const [isStoriesPaused, setIsStoriesPaused] = useState(true);
+function SeeMore() {
+  return <div>see more</div>;
+}
 
+const image = {
+  display: "block",
+  maxWidth: "100%",
+  borderRadius: 4,
+};
+const contentStyle = {
+  background: "salmon",
+  width: "100%",
+  height: "100%",
+  padding: 20,
+  paddingTop: 60,
+  color: "white",
+};
+
+const StorySlider = () => {
   const stories = [
-    "https://images.pexels.com/photos/774861/pexels-photo-774861.jpeg?auto=compress&cs=tinysrgb&w=1000",
     {
-      url: "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
-      duration: 5000,
-      seeMore: SeeMore,
-    },
-    {
-      url: "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
-      duration: 2000,
-      seeMore: ({ close }) => {
-        return <div onClick={close}>Hello, click to close this.</div>;
+      content: ({ action, isPaused }) => {
+        return (
+          <div style={contentStyle}>
+            <h1>Hey All 👋</h1>
+            <h1>Check shikha's journey story.</h1>
+
+            {/* <pre>
+              <code style={code}>here they are 😄 -></code>
+            </pre> */}
+            <img
+              style={image}
+              src="https://i.ibb.co/fY1DmQW/8aacdef9ba37db60c7a96271877cfbb5.jpg"
+            ></img>
+            <h4>stories creted by Cubestop travel app</h4>
+          </div>
+        );
       },
     },
     {
-      url: "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
-      duration: 5000,
+      url: "https://picsum.photos/1080/1920",
+      seeMore: <SeeMore />,
       header: {
-        // heading: "Mohit Karekar",
-        subheading: "Posted 30m ago",
+        heading: "Mohit Karekar",
+        subheading: "Posted 5h ago",
+        profileImage: "https://picsum.photos/1000/1000",
+      },
+    },
+    {
+      url: "https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN",
+      header: {
+        heading: "Mohit Karekar",
+        subheading: "Posted 32m ago",
+        profileImage: "https://picsum.photos/1080/1920",
+      },
+    },
+    {
+      url: "https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg",
+      header: {
+        heading: "mohitk05/react-insta-stories",
+        subheading: "Posted 32m ago",
         profileImage:
-          "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
+          "https://avatars0.githubusercontent.com/u/24852829?s=400&v=4",
       },
     },
     {
-      url: "https://images.pexels.com/photos/640809/pexels-photo-640809.jpeg?auto=compress&cs=tinysrgb&w=1000",
-      duration: 2000,
-      seeMore: ({ close }) => {
-        return <div onClick={close}>Hello, click to close this.</div>;
-      },
+      url: "https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4",
+      type: "video",
+      duration: 1000,
     },
     {
-      url: "https://exit109.com/~dnn/clips/RW20seconds_1.mp4#t=.1",
-      duration: 5000, // ignored
+      url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+      type: "video",
+      seeMore: <SeeMore />,
+    },
+    {
+      url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
       type: "video",
     },
-    {
-      content: (props) => (
-        <div style={{ background: "pink", padding: 20 }}>
-          <h1 style={{ marginTop: "100%", marginBottom: 0 }}>🌝</h1>
-          <h1 style={{ marginTop: 5 }}>A custom title can go here.</h1>
-        </div>
-      ),
-    },
+    "https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80",
   ];
 
-  const handleStoryStart = () => {
-    // Callback when a story starts
-    console.log("Story started");
-  };
-
-  const handleStoryEnd = () => {
-    // Callback when a story ends
-    console.log("Story ended");
-  };
-
-  const handleAllStoriesEnd = () => {
-    // Callback when all stories have ended
-    console.log("All stories ended");
-    // You can close the modal or perform any other action here
-  };
-
-  useEffect(() => {
-    // Set isStoriesPaused to false when the modal is opened
-    setIsStoriesPaused(false);
-
-    // Clean up and pause stories when the modal is closed
-    return () => {
-      setIsStoriesPaused(true);
-    };
-  }, []);
-
   return (
-    <div className="h-screen w-full">
-      <Stories
-        stories={stories}
-        height="100%"
-        loop={false}
-        defaultInterval={1500}
-        isPaused={isStoriesPaused}
-        onStoryStart={handleStoryStart}
-        onStoryEnd={handleStoryEnd}
-        onAllStoriesEnd={handleAllStoriesEnd}
-        // Add other props as needed
-      />
-    </div>
+    <ReactInstaStories
+      stories={stories}
+      defaultInterval={1500}
+      width={432}
+      height={768}
+    />
   );
-}
+};
+
+export default StorySlider;
