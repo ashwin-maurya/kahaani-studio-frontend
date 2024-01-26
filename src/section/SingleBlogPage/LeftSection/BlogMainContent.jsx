3e726714774a85eff8454aae20814bf690399ac8
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-01-26 11:51:55 +0530

    Backend

--- src/section/SingleBlogPage/LeftSection/BlogMainContent.jsx
+++ src/section/SingleBlogPage/LeftSection/BlogMainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MainContent({ content }) {
   return (
-    <article className="tracking- mx-auto mt-12 pl-10 font-HankenGrotesk text-xl leading-relaxed text-gray-700 max-md:text-lg">
+    <article className="blogContent mx-auto mt-12 max-w-[100vw] text-wrap pl-10 font-HankenGrotesk text-xl leading-relaxed text-gray-700 max-md:max-w-full max-md:text-lg max-sm:m-2 max-sm:pl-0">
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </article>
   );
--- src/section/SingleBlogPage/LeftSection/ShareModal.jsx
+++ src/section/SingleBlogPage/LeftSection/ShareModal.jsx
@@ -11,7 +11,7 @@ export default function ShareModal() {
   return (
     <>
       <div
-        className={`fixed bottom-0 left-0 z-50 overflow-hidden  text-[1.7vw] text-gray-100 max-md:hidden`}
+        className={`maxmd:hidden fixed bottom-0 left-0 z-50  overflow-hidden text-[1.7vw] text-gray-100`}
       >
         <div
           className={` flex cursor-pointer flex-col items-center justify-center bg-gray-800 p-1 shadow transition-all duration-500 ease-out`}
